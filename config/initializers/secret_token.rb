36f2ea81bb27777b3ca80e4021fe170562c28812
Sam Shaaban <sam@nurelm.com>
2014-04-13 05:25:19 -0400

    Have to add this for Heroku.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opentree::Application.config.secret_key_base = 'a17f18958fe95d0d8a1016a1b36e6761dc9d28a1d3ee12d6a42fc51957e31e4dee2935c507d89306c11e75641e307d317f4aaaea79de04c046a39d18dc98c5d3'
