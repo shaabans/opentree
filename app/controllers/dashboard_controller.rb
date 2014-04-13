243ab41eef46f23d62c8f33713d3d6431060ba2b
Sam Shaaban <sam@nurelm.com>
2014-04-13 11:57:24 -0400

    Got rid of limit.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @trees = get_flickr_list.first(5)
+    @trees = get_flickr_list
     @marker_hash = Gmaps4rails.build_markers(@trees) do |tree, marker|
       marker.lat tree.latitude
       marker.lng tree.longitude
