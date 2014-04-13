2d981d4d2444150efa0ca91903319035a015c1fa
Sam Shaaban <sam@nurelm.com>
2014-04-13 00:42:43 -0400

    Minor cosmetic updates.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @trees = get_flickr_list
+    @trees = get_flickr_list.first(5)
     @marker_hash = Gmaps4rails.build_markers(@trees) do |tree, marker|
       marker.lat tree.latitude
       marker.lng tree.longitude
